2a1e9d9757bf5b59e86b16df3b72ca1746617da8
Gintokix69 <stephenyang17@gmail.com>
2022-06-13 22:25:30 +0800

    image admin

--- Home Page/ProductCard.php
+++ Home Page/ProductCard.php
@@ -28,7 +28,7 @@ function productCard($productID, $productName, $productQty, $portfolioModalNum, 
                                                 <div class=\"modal-body\"  >
                                                     <!-- Project details-->
                                                     <h2 class=\"text-uppercase\">$productName</h2>
-                                                    <p class=\"item-intro text-muted\">Lorem ipsum dolor sit amet consectetur.</p>
+                                                    <p class=\"item-intro text-muted\"></p>
                                                     <img class=\"img-fluid d-block mx-auto\" src=\"../View Product/photos/$productImg\" alt=\"...\" />
                                                     
                                                     <form action = \"../View Product/Product Page.php\" method = \"GET\">
