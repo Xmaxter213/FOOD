6b49eb369b236fe16830615e5ddf4f98fb88703a
Gintokix69 <stephenyang17@gmail.com>
2022-07-18 21:45:13 +0800

    aba di ko na alam

--- View Product/php layouts/ProductPageLayout2.php
+++ View Product/php layouts/ProductPageLayout2.php
@@ -3,7 +3,7 @@
 function productPageLayout2($productName, $productPrice, $productQty, $productID){
     $element ="
     
-    <label for=\"productName\"><h1>$productName</h1></label> <br>
+    <label for=\"productName\"><h2>$productName</h2></label> <br>
     <label for=\"max\"> <h6>Product Left: $productQty</h6></label> <br>
     <label for=\"productPrice\">Price: $productPrice</label><br>
     <label for=\"quantity\">Quantity: </label>
