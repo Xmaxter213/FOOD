1836d44febe5d27e093fc8faf06d000ca749ac63
Gintokix69 <stephenyang17@gmail.com>
2022-07-18 20:09:28 +0800

    fixed history page

--- History Page/HistoryPage.php
+++ History Page/HistoryPage.php
@@ -53,7 +53,7 @@ require_once('../Connection/Connection.php');
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
                         <li class="nav-item"><a class="nav-link" href="../Home Page/index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="#portfolio">Product</a></li>
+                        <li class="nav-item"><a class="nav-link" href="../Home Page/index.php">Product</a></li>
                         <!--<li class="nav-item"><a class="nav-link" href="#team">Team</a></li>-->
                         <!--<li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>-->
                         <li class="nav-item "><a class="nav-link active" href="#contact">History</a></li>
